289e828825fb5508c35199de68e1672c5df6655c
adamure <adamqure@icloud.com>
2018-02-08 15:57:22 -0700

    Initial Commit

---
+++ Palindrome Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Palindrome Calculator
+//
+//  Created by Adam Ure on 2/8/18.
+//  Copyright © 2018 OpenEdge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
